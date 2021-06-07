2fd228f41c0a420fec8a5b298938f96cf97d53a5
Darshan K <darshankgowda1995@gmail.com>
2021-06-07 14:55:01 +0530

    registation and login UI compoent changes

--- client/ecomsite/src/components/Login.jsx
+++ client/ecomsite/src/components/Login.jsx
@@ -1,29 +1,103 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-//import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const Login = (props) =>{
-    return (<div>
-        <h1>Login Component</h1>
-        <form noValidate autoComplete="off">
-            <Card className="card">
-                <CardContent>
-                    <Grid spacing={3}>
-                        <Grid  item xs={12}>
-                        <Paper className="input-field" ><TextField id="user-name" label="User Name" variant="outlined" /></Paper>
-                        </Grid>
-                        <Grid item xs={12}>
-                        <Paper className="input-field"><TextField id="password" label="Password" variant="outlined" type="password" /></Paper>
-                        </Grid>
-                    </Grid>  
-                </CardContent>
-            </Card>
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+   // backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <a href="Registration.jsx" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </a>
+            </Grid>
+          </Grid>
         </form>
-</div>    );
-};
+      </div>
+      <Box mt={8}>
 
-export default Login;
+      </Box>
+    </Container>
+  );
+}
