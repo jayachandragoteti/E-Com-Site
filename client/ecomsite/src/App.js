fc6461d8dc65a2fa7a925686b9315edc34b9e1d4
Darshan K <darshankgowda1995@gmail.com>
2021-06-07 14:55:01 +0530

    registation and login UI compoent changes

--- client/ecomsite/src/App.js
+++ client/ecomsite/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import Login from "./components/Login"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Registration from './components/Registration'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <Login>
-     </ Login>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/login"><Login /></Route>
+          <Route path="/register"><Registration /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
 
 export default App;
