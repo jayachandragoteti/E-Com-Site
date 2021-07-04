719194255121a65ca0a8532a676cdbe4a85cb91f
Meghana Sakalabhaktula <meghana9111@gmail.com>
2021-07-04 12:22:41 +0530

    warning removed

--- client/ecomsite/src/components/Header.jsx
+++ client/ecomsite/src/components/Header.jsx
@@ -8,7 +8,7 @@ import InputBase from '@material-ui/core/InputBase';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
+
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import ShoppingCart from '@material-ui/icons/ShoppingCart';
